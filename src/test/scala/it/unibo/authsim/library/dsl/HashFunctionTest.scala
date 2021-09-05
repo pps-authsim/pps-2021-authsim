bfdd29c0fbe6e41f2c81d8c64e8752b743963373
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-05 21:27:13 +0200

    added untracked file

--- src/test/scala/it/unibo/authsim/library/dsl/HashFunctionTest.scala
+++ src/test/scala/it/unibo/authsim/library/dsl/HashFunctionTest.scala
@@ -4,22 +4,33 @@ import it.unibo.authsim.library.dsl.HashFunction
 import org.scalatest.wordspec.AnyWordSpec
 
 class HashFunctionTest extends AnyWordSpec {
-  var password="foo"
+  var password = "foo"
   var hashFunctionSHA = new HashFunction.SHA
   var hashFunctionMD5 = new HashFunction.MD5
   var hashedValueSHA = "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"
   var hashedValueMD5 = "acbd18db4cc2f85cedef654fccc4a4d8"
+  def is = afterWord("is")
 
-  "A Password hashed with SHA algorithm" should{
-    "returned the hashed value" in {
+  "A password" when {
+    "hashed with the SHA algorithm" should{
+    "return the hashed value" which is {
       assert(hashFunctionSHA.hash(password) == hashedValueSHA)
-
     }
+    "and is not " in{
+      assert(hashFunctionSHA.hash(password) != "")
+    }
+    markup("The test for the **SHA** finished!")
   }
+}
 
-  "A Password hashed with MD5 algorithm" should{
-    "returned the hashed value" in {
-      assert(hashFunctionMD5.hash(password) == hashedValueMD5)
+  "A password" when {
+    "hashed with MD5 algorithm" should {
+      "return the hashed value" which is {
+        assert(hashFunctionMD5.hash(password) == hashedValueMD5)
+      }
+      "and is not " in{
+        assert(hashFunctionMD5.hash(password) != "")
+      }
     }
   }
 

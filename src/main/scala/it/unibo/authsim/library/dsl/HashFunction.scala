e89186f49c66fa445624038e1eaba328f8cd1cb8
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-06 14:39:46 +0200

    added methods for sha256 and 384 encryption

--- src/main/scala/it/unibo/authsim/library/dsl/HashFunction.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/HashFunction.scala
@@ -3,6 +3,8 @@ package it.unibo.authsim.library.dsl
 import java.nio.charset.StandardCharsets
 import org.apache.commons.codec.binary.Hex
 import org.apache.commons.codec.digest.DigestUtils
+import com.google.common.hash.Hashing
+import com.google.common.io.BaseEncoding
 
 trait HashFunction {
   def hash(str: String): String
@@ -11,6 +13,12 @@ trait HashFunction {
 object HashFunction {
   case class SHA1() extends HashFunction {
     override def hash(password: String): String = DigestUtils.shaHex(password.getBytes(StandardCharsets.UTF_8))
+  }
+  case class SHA256() extends HashFunction {
+    override def hash(password: String): String = Hashing.sha256().hashString(password, StandardCharsets.UTF_8).toString
+  }
+  case class SHA384() extends HashFunction {
+    override def hash(password: String): String = Hashing.sha384().hashString(password, StandardCharsets.UTF_8).toString
   }
   case class MD5() extends HashFunction {
     override def hash(password: String): String = DigestUtils.md5Hex(password.getBytes(StandardCharsets.UTF_8))

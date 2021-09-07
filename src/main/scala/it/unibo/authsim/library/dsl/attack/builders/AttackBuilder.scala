0a5de30938b19f9dab473521cd82ab8664066af9
Alex Speranza <alex.speranza@studio.unibo.it>
2021-09-07 10:35:31 +0200

    Adds attack builder trait and stub attack trait

---
+++ src/main/scala/it/unibo/authsim/library/dsl/attack/builders/AttackBuilder.scala
@@ -0,0 +1,30 @@
+package it.unibo.authsim.library.dsl.attack.builders
+
+import org.joda.time.Duration
+import it.unibo.authsim.library.dsl.attack.logspecification.LogSpec
+
+trait AttackBuilder {
+  private var logSpecification: Option[LogSpec] = Option.empty
+  private var timeout: Option[Duration] = Option.empty
+
+  final def getLogSpecification() = this.logSpecification
+  final def getTimeout() = this.timeout
+
+  def log(logSpecification: LogSpec): AttackBuilder = {
+    this.logSpecification = Option(logSpecification)
+    this
+  }
+
+  def timeout(time: Duration): AttackBuilder = {
+    this.timeout = Option(time)
+    this
+  }
+
+  def save(): Attack
+
+  def executeNow(): Unit
+}
+
+trait Attack {
+  def start(): Unit
+}

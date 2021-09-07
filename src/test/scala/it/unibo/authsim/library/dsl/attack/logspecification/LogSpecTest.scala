8016f54c550df305d7a212e63dcc2e92669b53c6
Alex Speranza <alex.speranza@studio.unibo.it>
2021-09-07 10:34:18 +0200

    Adds LogSpec class and tests

---
+++ src/test/scala/it/unibo/authsim/library/dsl/attack/logspecification/LogSpecTest.scala
@@ -0,0 +1,33 @@
+package it.unibo.authsim.library.dsl.attack.logspecification
+
+import org.scalatest.wordspec.AnyWordSpec
+import it.unibo.authsim.library.dsl.attack.logspecification.*
+import it.unibo.authsim.library.dsl.stub.Logger
+
+class LogSpecTest extends AnyWordSpec {
+  val myLogger: Logger = new Logger() {}
+  val is = afterWord("is")
+
+  "A LogCategory" when {
+    "directed to a Logger" should {
+      "be implicitly converted into a LogSpec" in {
+        assert((LogCategory.ALL to myLogger).isInstanceOf[LogSpec])
+      }
+    }
+    "listed with and" can {
+      "be concatenated" in {
+        assert((LogCategory.TIME and LogCategory.SUCCESS to myLogger).isInstanceOf[LogSpec])
+      }
+    }
+    "createb by a list of categories" should {
+      "contain the selected categories" in {
+        val categorySet = (LogCategory.ALL to myLogger).getCategories()
+        assert(categorySet.size == 1 && categorySet(LogCategory.ALL))
+      }
+      "not contain repeated values" in {
+        val categorySet = (LogCategory.TIME and LogCategory.SUCCESS and LogCategory.TIME to myLogger).getCategories()
+        assert(categorySet.size == 2)
+      }
+    }
+  }
+}

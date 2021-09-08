0dd4950ac449df81f1d12e097aad5a4d5183d6c1
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-08 19:48:56 +0200

    refactor: delete branches

--- src/main/scala/it/unibo/authsim/library/dsl/Logger.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/Logger.scala
@@ -1,17 +1,14 @@
 package it.unibo.authsim.library.dsl
 import java.util.Date
 
-trait Logger {
+trait Logger 
   def receiveExecutionTime(executionTime: Long): Unit
   def receiveStatistics(map: Map [String, String]): Unit
   def receiveCracked(flag: Boolean): Unit
-}
 
-object Logger{
+object Logger
   def apply(): Logger = LoggerImpl()
-  case class LoggerImpl() extends Logger {
+  case class LoggerImpl() extends Logger 
     override def receiveExecutionTime(executionTime:  Long): Unit= println(s"Execution time: $executionTime")
     override def receiveStatistics(statistics: Map [String, String]): Unit = println(s"Statistics:${statistics.toString()}")
     override def receiveCracked(flag: Boolean): Unit = println(if(flag) then "The attak succeded: password cracked" else "The attack did not succeded")
-  }
-}

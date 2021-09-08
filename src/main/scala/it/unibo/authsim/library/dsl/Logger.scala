245212d4f4e6b21a3862907cc41d76a9eadebbbb
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-08 18:16:28 +0200

    update Logger test

--- src/main/scala/it/unibo/authsim/library/dsl/Logger.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/Logger.scala
@@ -12,16 +12,6 @@ object Logger{
   case class LoggerImpl() extends Logger {
     override def receiveExecutionTime(executionTime:  Long): Unit= println(s"Execution time: $executionTime")
     override def receiveStatistics(statistics: Map [String, String]): Unit = println(s"Statistics:${statistics.toString()}")
-    override def receivelsCracked(flag: Boolean): Unit = println(if(flag) then "The attak succeded: password cracked" else "The attack did not succeded")
+    override def receiveCracked(flag: Boolean): Unit = println(if(flag) then "The attak succeded: password cracked" else "The attack did not succeded")
   }
-}
-
-object LoggerTest extends App{
-  import it.unibo.authsim.library.dsl.Logger.LoggerImpl
-  val logger= new LoggerImpl
-  val time = new Date()
-
-  logger.receivelsCracked(true)
-  logger.receiveStatistics(Map.empty)
-  logger.receiveExecutionTime(time.getTime)
 }
--- src/test/scala/it/unibo/authsim/library/dsl/LoggerTest.scala
+++ src/test/scala/it/unibo/authsim/library/dsl/LoggerTest.scala
@@ -1,18 +1,15 @@
 package it.unibo.authsim.library.dsl
 import it.unibo.authsim.library.dsl.Logger.LoggerImpl
 import org.scalatest.funsuite.AnyFunSuite
+import java.util.Calendar
 
 import java.util.Date
 
-class LoggerTest extends AnyFunSuite {
+object LoggerTest extends App{
+    val logger= new LoggerImpl
 
-  test("An logger should print readable string") {
-    var logger = new LoggerImpl
-    var date = new Date
-    var map = Map.empty("prova","prova")
-
-    logger.receivelsCracked(true)
-    logger.receiveStatistics(map)
-    logger.receiveExecutionTime(date.getTime)
+    logger.receiveCracked(true)
+    logger.receiveStatistics(Map.empty)
+    logger.receiveExecutionTime(20)
   }
-}
+

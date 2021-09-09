35eb7fcfd58195e8496a13cedddc9ac10ce0a714
Alex Speranza <alex.speranza@studio.unibo.it>
2021-09-09 11:42:29 +0200

    Adds a function to set the hash function to use in the attack

--- src/main/scala/it/unibo/authsim/library/dsl/attack/builders/OfflineAttackBuilder.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/attack/builders/OfflineAttackBuilder.scala
@@ -1,12 +1,14 @@
 package it.unibo.authsim.library.dsl.attack.builders
 
-import it.unibo.authsim.library.dsl.Proxy
+import it.unibo.authsim.library.dsl.{HashFunction, Proxy}
 
 trait OfflineAttackBuilder extends AttackBuilder {
   private var target: Proxy = null
+  private var hashFunction: HashFunction = null
   private var numberOfWorkers: Int = 1
 
   final def getTarget() = this.target
+  final def getHashFunction() = this.hashFunction
   final def getNumberOfWorkers = this.numberOfWorkers
 
   def against(target: Proxy): OfflineAttackBuilder = {
@@ -19,6 +21,10 @@ trait OfflineAttackBuilder extends AttackBuilder {
     this
   }
 
+  def hashingWith(hashFunction: HashFunction): OfflineAttackBuilder = {
+    this.hashFunction = hashFunction
+    this
+  }
 }
 
 trait OfflineAttack extends Attack {

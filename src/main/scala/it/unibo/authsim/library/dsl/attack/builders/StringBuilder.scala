1f61d2d45f322fead80e6408d53f9c32aea84c66
Alex Speranza <alex.speranza@studio.unibo.it>
2021-09-10 15:15:43 +0200

    Changes the string builder to be a monitor and a lazy provider of strings

--- src/main/scala/it/unibo/authsim/library/dsl/attack/builders/StringBuilder.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/attack/builders/StringBuilder.scala
@@ -1,29 +1,38 @@
 package it.unibo.authsim.library.dsl.attack.builders
 
 /**
- * Utility class which allows to generate a list of strings given an alphabet.
- * The size of the alphabet has more influence on the execution time rather than the length of the requested string,
- * so beware of big alphabets, they are likely to OOM-kill the execution.
+ * A ConcurrentStringProvider lazily generates all the possible string given an alphabet and a maximum length.
+ * It also ensure mutual exclusion for retrieving the next string.
+ *
+ * @param alphabet The alphabet to use to generate the strings
+ * @param maxLength The maximum length of the strings to generate
  */
-class StringBuilder(private val alphabet: List[String]) {
-  /**
-   * Generate all the strings with a specific length
-   * @param length The length of the strings
-   * @return A lazy list with the generated strings
-   */
-  def getAllStringsWithLength(length: Int): LazyList[String] = length match {
-    case 0 => LazyList("")
-    case 1 => LazyList.from(alphabet.toArray)
-    case _ => getAllStringsWithLength(length - 1).flatMap(s => alphabet.map(symbol => s + symbol))
-  }
+class ConcurrentStringProvider(val alphabet: List[String], val maxLength: Int) {
+  private var stringList: LazyList[String] = getAllStringsUpToLength(maxLength)
 
   /**
-   * Get all the strings long up to the input length.
-   * @param maxLength The maximum length of a string
-   * @return The lazy list with all the strings generated
+   * Returns the next available string, if possible.
+   * @return an Option containing the next string generated if possible, else Option.empty
    */
-  def getAllStringsUpToLength(maxLength: Int): LazyList[String] = maxLength match {
-    case 0 | 1 => getAllStringsWithLength(maxLength)
+  def getNextString(): Option[String] = this.synchronized {
+    val next = stringList.headOption
+    stringList = stringList.drop(1)
+    next
+  }
+
+  private def getAllStringsUpToLength(maxLength: Int): LazyList[String] = maxLength match {
+    case 0 => LazyList("")
+    case 1 => LazyList.from(alphabet.toArray)
     case _ => LazyList.from(alphabet.toArray) ++ getAllStringsUpToLength(maxLength - 1).flatMap(s => alphabet.map(symbol => s + symbol))
   }
+}
+
+/**
+ * This companion object provides some shorthands for standard alphabets.
+ */
+object ConcurrentStringProvider {
+  def lowercaseLetters: List[String] = List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+  def uppercaseLetters: List[String] = lowercaseLetters.map(l => l.toUpperCase)
+  def numbers: List[String] = (0 to 9).map(i => i.toString).toList
+  def symbols: List[String] = List(" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~")
 }
--- src/test/scala/it/unibo/authsim/library/dsl/attack/builders/StringBuilderTest.scala
+++ src/test/scala/it/unibo/authsim/library/dsl/attack/builders/StringBuilderTest.scala
@@ -6,53 +6,21 @@ import java.util.stream.Collectors
 
 class StringBuilderTest extends AnyWordSpec {
   val alphabet = List("a", "b")
-  val stringBuilder = new StringBuilder(alphabet)
-  val alphabet2LengthStrings = List("aa", "ab", "ba", "bb")
+  val concurrentStringProvider = new ConcurrentStringProvider(alphabet, 2)
 
-  "A string builder" when {
-    "generating 0-length strings" should {
-      "return a list with an empty string" in {
-        assert(stringBuilder.getAllStringsWithLength(0) == List(""))
+  "A string provider" when {
+    "the max length is > 0" should {
+      "have available strings" in {
+        assert(!concurrentStringProvider.getNextString().isEmpty)
       }
     }
-
-    "generating 1-length strings" should {
-      "return a list with the symbols of the alphabet" in {
-        assert(stringBuilder.getAllStringsWithLength(1) == alphabet)
-      }
-    }
-
-    "generating 2-length strings" should {
-      "return a list with the combinations of the symbols of the alphabet" in {
-        assert(stringBuilder.getAllStringsWithLength(2) == alphabet2LengthStrings)
-      }
-    }
-
-    "generating strings up to 0 length" should {
-      "return a list with an empty string" in {
-        assert(stringBuilder.getAllStringsUpToLength(0) == List(""))
-      }
-    }
-
-    "generating strings up to 1 length" should {
-      "return the alphabet symbol list" in {
-        assert(stringBuilder.getAllStringsUpToLength(1) == alphabet)
-      }
-    }
-
-    "generating strings up to 2 length" should {
-      "return the alphabet symbol list and their combination" in {
-        assert(stringBuilder.getAllStringsUpToLength(2) == (alphabet ++ alphabet2LengthStrings))
+    "all the strings are consumed" should {
+      "return an empty value" in {
+        while !concurrentStringProvider.getNextString().isEmpty do
+          ;
+        end while
+        assert(concurrentStringProvider.getNextString().isEmpty)
       }
     }
   }
-
-  /*
-    This is the "usual" alphabet for passwords:
-    val fullAlphabet = List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-                       .flatMap(c => List(c, c.toUpperCase))
-                       ++ List(" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~")
-    val fullStringBuilder = new StringBuilder(fullAlphabet)
-    println("Alphabet length: " + fullAlphabet.length)
-  */
 }

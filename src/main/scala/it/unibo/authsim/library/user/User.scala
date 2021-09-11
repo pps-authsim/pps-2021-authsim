92ea9c74075dbd8512318996f24fa95b5ec845b6
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-11 22:27:30 +0200

    trivial implementation of userbuilder interfaces

--- src/main/scala/it/unibo/authsim/library/user/User.scala
+++ src/main/scala/it/unibo/authsim/library/user/User.scala
@@ -3,3 +3,22 @@ package it.unibo.authsim.library.user
 trait User:
   val username: String
   val password: String
+
+
+trait UserGenerator extends UserBuilder with UserIDPolicy with PasswordPolicy with CredentialPolicy with Policy
+
+trait UserBuilder:
+  def generate(user: User)= Option.empty[User]
+
+trait UserAutoBuilder:
+  def generate(): User
+  def times(number: Int): Seq[User]
+//non sono troppo convinta del nome del metodo
+
+trait UserIDPolicy
+
+trait PasswordPolicy
+
+trait CredentialPolicy
+
+trait Policy

887d2bac04012aee86ac62aa2a5b80659d395f11
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-14 22:14:55 +0200

    make decrypt/encrypt methods works with string

--- src/main/scala/it/unibo/authsim/library/dsl/Encryption4.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/Encryption4.scala
@@ -7,6 +7,8 @@ import javax.crypto.spec.SecretKeySpec
 import javax.crypto.{Cipher, Mac}
 import javax.crypto.spec.IvParameterSpec
 
+trait Encryption
+
 object AESCTR:
   //DES,
   def encrypt(input: Array[Byte], key: Array[Byte], iv: Array[Byte]): Array[Byte] =
@@ -24,6 +26,29 @@ object AESCTR:
     cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
     cipher.doFinal(input)
 
+  def encrypt2(password: String, key: String, iv:String): String=
+    val input = password.getBytes("UTF-8")
+    val key2 = Hex.decodeHex(key.toCharArray)
+    val iv2 = Hex.decodeHex(iv.toCharArray)
+    val keySpec = new SecretKeySpec(key2, "AES")
+    val ivSpec = new IvParameterSpec(iv2)
+    val cipher = Cipher.getInstance("AES/CTR/NoPadding")
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec)
+    cipher.doFinal(input)
+    new String(input, "UTF-8")
+
+  def decrypt2(passwordEcrypted: String, key: String, iv:String): String=
+    val input = passwordEcrypted.getBytes("UTF-8")
+    val key2 = Hex.decodeHex(key.toCharArray)
+    val iv2 = Hex.decodeHex(iv.toCharArray)
+    val keySpec = new SecretKeySpec(key2, "AES")
+    val ivSpec = new IvParameterSpec(iv2)
+    val cipher = Cipher.getInstance("AES/CTR/NoPadding")
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
+    cipher.doFinal(input)
+    new String(input, "UTF-8")
+
+
 
 object App:
   def main(args: Array[String]): Unit =
@@ -33,6 +58,11 @@ object App:
     val iv = Hex.decodeHex("01020304050607080910111201010101".toCharArray)
     val encrypted = AESCTR.encrypt(input, key, iv)
     val decrypted = AESCTR.decrypt(encrypted, key, iv)
-    
+
     println("input: " + new String(input, "UTF-8"))
     println("decrypted: " + new String(decrypted, "UTF-8"))
+
+    val encrypted2 = AESCTR.encrypt2("my super secret input!!!", "000102030405060708090a0b0c0d0e0f", "01020304050607080910111201010101")
+    val decrypted2 = AESCTR.decrypt2(encrypted2, "000102030405060708090a0b0c0d0e0f", "01020304050607080910111201010101")
+
+    println("decrypted2: " + decrypted2)

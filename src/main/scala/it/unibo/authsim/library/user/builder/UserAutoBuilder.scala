a913f98da263148c701867164fa2cb1b198e838a
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-16 09:45:33 +0200

    update implementation checkpolicy()

--- src/main/scala/it/unibo/authsim/library/user/builder/UserAutoBuilder.scala
+++ src/main/scala/it/unibo/authsim/library/user/builder/UserAutoBuilder.scala
@@ -1,5 +1,25 @@
 package it.unibo.authsim.library.user.builder
 
-class UserAutoBuilder {
+import it.unibo.authsim.library.dsl.policy.checkers.StringPolicyChecker
+import it.unibo.authsim.library.dsl.policy.model.StringPolicies.{CredentialPolicy, PasswordPolicy, UserIDPolicy}
+import it.unibo.authsim.library.user.model.User
+import scala.util.Random
+case class UserAutoBuilder(val credentialPolicy:Seq[CredentialPolicy]= Seq.empty): //extends UserBuilder(username, password):
+  private def genrateRandomString(length:Int=5): String=
+    Random.alphanumeric.filter(_.isDigit).take(length).mkString
 
-}
+  def build(): Option[User]=
+    var result1=true
+    var result2=true
+    if(credentialPolicy.isEmpty) then
+      val user: User = User(genrateRandomString(), genrateRandomString())
+      Some(user)
+    else None
+      //for( e <-credentialPolicy)
+        //if(e.isInstanceOf[PasswordPolicy])then
+          //e.asInstanceOf[PasswordPolicy]
+          //result1=StringPolicyChecker(e.asInstanceOf[PasswordPolicy]) check password
+        //else if (e.isInstanceOf[UserIDPolicy])then result2=StringPolicyChecker(e.asInstanceOf[UserIDPolicy]) check username
+        //None
+
+
--- src/main/scala/it/unibo/authsim/library/user/builder/UserBuilder.scala
+++ src/main/scala/it/unibo/authsim/library/user/builder/UserBuilder.scala
@@ -53,12 +53,19 @@ case class UserBuilder(val username:String, val password:String, val credentialP
 case class UserBuilder2(val username:String, val password:String, val credentialPolicy:Seq[CredentialPolicy]= Seq.empty): //extends UserBuilder(username, password):
   //_credentialPolicies=credentialPolicy
   private def checkPolicy(): Boolean=
-    var result1=true
-    var result2=true
-    for( e <-credentialPolicy)
-      if(e.isInstanceOf[PasswordPolicy])then result1=StringPolicyChecker(e.asInstanceOf[PasswordPolicy]) check password
-      else if (e.isInstanceOf[UserIDPolicy])then result2=StringPolicyChecker(e.asInstanceOf[UserIDPolicy]) check username
-    result1&&result2
+    !credentialPolicy.filter(e=> (e.isInstanceOf[PasswordPolicy]|| e.isInstanceOf[UserIDPolicy])).map(e=>
+      if(e.isInstanceOf[PasswordPolicy]) then
+        StringPolicyChecker(e.asInstanceOf[PasswordPolicy]) check password
+      else if (e.isInstanceOf[UserIDPolicy]) then
+        StringPolicyChecker(e.asInstanceOf[UserIDPolicy]) check username
+    ).contains(false)
+    /*
+        var result1=true
+        var result2=true
+        for( e <-credentialPolicy)
+          if(e.isInstanceOf[PasswordPolicy])then result1=StringPolicyChecker(e.asInstanceOf[PasswordPolicy]) check password
+          else if (e.isInstanceOf[UserIDPolicy])then result2=StringPolicyChecker(e.asInstanceOf[UserIDPolicy]) check username
+        result1&&result2*/
 
   def build(): Option[User]=
     if(checkPolicy()) then

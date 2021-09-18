2e4dde0e4a02c23a1e686584e9854274d451ea5b
Alex Speranza <alex.speranza@studio.unibo.it>
2021-09-18 10:44:07 +0200

    Adds StatisticsConsumer trait, with a simple implementation in the companion object

---
+++ src/main/scala/it/unibo/authsim/library/dsl/StatisticsConsumer.scala
@@ -0,0 +1,15 @@
+package it.unibo.authsim.library.dsl
+
+import it.unibo.authsim.library.dsl.attack.statistics.Statistics
+
+trait StatisticsConsumer extends Consumer[Statistics]:
+  override def consume(consumable: Statistics): Unit
+
+object StatisticsConsumer:
+  def apply(): StatisticsConsumer = BasicStatisticsConsumer()
+
+  private case class BasicStatisticsConsumer() extends StatisticsConsumer:
+    override def consume(consumable: Statistics): Unit =
+      println("Attempts: " + consumable.attempts)
+      println("Elapsed time: " + consumable.elapsedTime.toMillis + " ms")
+      println("Breached credentials: " + consumable.successfulBreaches.map(u => u.username + " - " + u.password).reduce((u1, u2) => u1 + "\n" + u2))

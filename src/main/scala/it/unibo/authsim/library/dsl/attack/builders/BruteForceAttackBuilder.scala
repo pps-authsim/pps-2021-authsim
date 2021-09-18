d2230ceb9e5ba307247921d5ea367cb871d12008
Alex Speranza <alex.speranza@studio.unibo.it>
2021-09-18 17:48:01 +0200

    Adds documentation to BruteForceAttackBuilder; improves 'save' and 'executeNow' signatures

--- src/main/scala/it/unibo/authsim/library/dsl/attack/builders/BruteForceAttackBuilder.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/attack/builders/BruteForceAttackBuilder.scala
@@ -10,25 +10,49 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.TimeoutException
 import concurrent.ExecutionContext.Implicits.global
 
+/**
+ * A builder of bruteforce attacks.
+ */
 class BruteForceAttackBuilder extends OfflineAttackBuilder:
   private var alphabet: List[String] = null
   private var maximumLength = 1
 
+  /**
+   * Sets the alphabet to use for the attack.
+   *
+   * A bare bruteforce attack should have an alphabet with only 1-length symbols,
+   * while other attacks can use a different alphabet
+   * (for example, a dictionary attack uses the dictionary itself as the alphabet).
+   * @param alphabet The alphabet to use.
+   * @return The builder.
+   */
   def usingAlphabet(alphabet: List[String]): this.type =
     this.alphabet = alphabet
     this
 
+  /**
+   * @return The used alphabet.
+   */
   def getAlphabet(): List[String] = this.alphabet
 
+  /**
+   * Sets the maximum number of combinations of symbols from the alphabet.
+   * Defaults to 1 (which means the attack only uses the provided symbols, without combining them).
+   * @param maximumLength The maximum combination number.
+   * @return The builder.
+   */
   def maximumLength(maximumLength: Int): this.type =
     this.maximumLength = maximumLength
     this
 
+  /**
+   * @return The maximum combination number.
+   */
   def getMaximumLength: Int = this.maximumLength
 
-  def save(): BruteForceAttack = new BruteForceAttack(this.getTarget(), this.getHashFunction(), this.getAlphabet(), this.getMaximumLength, this.getStatisticsConsumer(), this.getTimeout(), this.getNumberOfWorkers)
+  override def save(): Attack = new BruteForceAttack(this.getTarget(), this.getHashFunction(), this.getAlphabet(), this.getMaximumLength, this.getStatisticsConsumer(), this.getTimeout(), this.getNumberOfWorkers)
 
-  def executeNow(): Unit = this.save().start()
+  override def executeNow(): Unit = this.save().start()
 
 class BruteForceAttack(private val target: Proxy, private val hashFunction: HashFunction, private val alphabet: List[String], private val maximumLength: Int, private val logTo: Option[StatisticsConsumer], private val timeout: Option[Duration], private val jobs: Int) extends OfflineAttack:
 

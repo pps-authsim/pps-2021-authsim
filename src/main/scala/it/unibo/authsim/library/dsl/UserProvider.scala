b0a25f1d6a27564d37218d2bb2ba954fe9bd3b47
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-23 11:52:34 +0200

    update UserProvider documentation: userInformations()

--- src/main/scala/it/unibo/authsim/library/dsl/UserProvider.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/UserProvider.scala
@@ -6,6 +6,11 @@ import it.unibo.authsim.library.user.model.UserInformation
  * Trait for a UserProvider: it provides a list of UserInformation with whom the client can interact;
  */
 trait UserProvider :
+  /**
+   * Getter for a list of user information.
+   *
+   * @return    a list of UserInformation
+   */
   def userInformations(): List[UserInformation]
 
 /**

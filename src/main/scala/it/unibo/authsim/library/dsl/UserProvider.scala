ebf18dbbba14578247c3eea535ccb8082b7c779f
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-23 11:56:19 +0200

    Refactor: remove the compation object of UserProvider trait

--- src/main/scala/it/unibo/authsim/library/dsl/UserProvider.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/UserProvider.scala
@@ -12,26 +12,3 @@ trait UserProvider :
    * @return    a list of UserInformation
    */
   def userInformations(): List[UserInformation]
-
-/**
- * Object for a UserProvider
- */
-object UserProvider:
-  /**
-   * Apply method for the object
-   *
-   * @return a UserProvider
-   */
-  def apply(): UserProvider= new UserProvider:
-    //NB Non sto a scrivere altro perchè non so quanto durerà sta variabile
-    /**
-     * Variable that represent a representation of a userInformations provided
-     */
-    private var _userInformations: List[UserInformation] = List.empty
-
-    /**
-     * Getter method for the userInformation 
-     * 
-     * @return a List of UserInformation
-     */
-    override def userInformations(): List[UserInformation]= _userInformations

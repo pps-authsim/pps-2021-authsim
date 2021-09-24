83951812af1659f2198760ef122b2e4e7cd6afa6
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-24 15:24:00 +0200

    refactor[UserBuilderTest]: delete asInstanceOf

--- src/test/scala/it/unibo/authsim/library/user/UserBuilderTest.scala
+++ src/test/scala/it/unibo/authsim/library/user/UserBuilderTest.scala
@@ -13,29 +13,28 @@ import it.unibo.authsim.library.user.builder.util.RandomStringGenerator
 
 
 class UserBuilderTest extends AnyWordSpec with should.Matchers{
-
   private val userIDPolicy: CredentialPolicy = UserIDPolicyBuilder() minimumLength 5 build
   private val passwordPolicy: CredentialPolicy = PasswordPolicyBuilder() minimumUpperChars 3 minimumLength 8 build
-  private val name: String= "name1"
-  private val shortName: String= "na"
-  private val password: String= "password1"
+  private val name: String= RandomStringGenerator.generateRandomString(5)
+  private val shortName: String= RandomStringGenerator.generateRandomString(2)
+  private val password: String= RandomStringGenerator.generateRandomString(8)
 
-  private val costumUserBuilder1 = UserCostumBuilder() withName(name) withPassword(password)
+  private val costumUserBuilder1:UserCostumBuilder = UserCostumBuilder() withName(name) withPassword(password)
   private val costumUser1 :Option[User]= costumUserBuilder1.build()
 
-  private val costumUserBuilder2 =  UserCostumBuilder() withName(name) withPassword(password) withPolicy(userIDPolicy)
+  private val costumUserBuilder2:UserCostumBuilder =  UserCostumBuilder() withName(name) withPassword(password) withPolicy(userIDPolicy)
   private val costumUser2:Option[User] = costumUserBuilder2.build()
 
-  private val costumUserBuilder3 =  UserCostumBuilder() withName(shortName) withPassword(password) withPolicy(userIDPolicy)
+  private val costumUserBuilder3:UserCostumBuilder =  UserCostumBuilder() withName(shortName) withPassword(password) withPolicy(userIDPolicy)
   private val costumUser3:Option[User] = costumUserBuilder3.build()
 
-  private val autoUserBuilder1= UserAutoBuilder() withPolicy(userIDPolicy)
-  private val autoUser1:User = autoUserBuilder1.asInstanceOf[UserAutoBuilder].build()
+  private val autoUserBuilder1:UserAutoBuilder= UserAutoBuilder() withPolicy(userIDPolicy)
+  private val autoUser1:User = autoUserBuilder1.build()
 
-  private val autoUserBuilder2= UserAutoBuilder() withPolicy(userIDPolicy) withPolicy(passwordPolicy)
-  private val autoUser2:User = autoUserBuilder2.asInstanceOf[UserAutoBuilder].build()
+  private val autoUserBuilder2:UserAutoBuilder= UserAutoBuilder() withPolicy(userIDPolicy) withPolicy(passwordPolicy)
+  private val autoUser2:User = autoUserBuilder2.build()
 
-  private val seq=autoUserBuilder1.asInstanceOf[UserAutoBuilder].numberOfUsers(2)
+  private val seq:Seq[User]=autoUserBuilder1.asInstanceOf[UserAutoBuilder].numberOfUsers(2)
   s"A user created with name '${name}' and password '{$password}'" should {
     "have name" in{
       costumUser1.get.username should be (name)
@@ -65,7 +64,17 @@ class UserBuilderTest extends AnyWordSpec with should.Matchers{
       assert(StringPolicyChecker(userIDPolicy.asInstanceOf[UserIDPolicy]) check (autoUser1.password))
     }
   }
-
+  println("seq"+ seq)
+  var listUser:List[String]= List.empty[String]
+  var listPassword:List[String]= List.empty[String]
+  seq.map(e=>
+    listUser= e.username +:listUser
+    listPassword= e.password +:listPassword
+  )
+  val number1=RandomStringGenerator.countDuplicates(listUser.toSeq)
+  val number2=RandomStringGenerator.countDuplicates(listPassword.toSeq)
+  println("List users " + (listUser, number1))
+  println("List passwords " +(listPassword, number2))
   s"User" should{
     "be able to create multiple users" in{
       assert(seq.length==2)

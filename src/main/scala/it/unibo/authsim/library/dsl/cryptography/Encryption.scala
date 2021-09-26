a19c3d6025cc68d3f95584911d601b3c8679850a
Giulia <giulia.brugnatti@studio.unibo.it>
2021-09-26 08:38:59 +0200

    add abstract factory for Hash and symmetric encryption algorithms

--- src/main/scala/it/unibo/authsim/library/dsl/cryptography/Encryption.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/cryptography/Encryption.scala
@@ -1,4 +1,7 @@
 package it.unibo.authsim.library.dsl.cryptography
+import it.unibo.authsim.library.dsl.cryptography.hash.HashFunction
+import it.unibo.authsim.library.dsl.cryptography.asymmetric.RSA
+import it.unibo.authsim.library.dsl.cryptography.symmetric.{AES, DES, CaesarCipher}
 
 import java.security.KeyPair
 import java.security.spec.{AlgorithmParameterSpec, KeySpec}
@@ -28,7 +31,31 @@ trait KeyGenerator:
 enum EncryptionMode:
     case Decryption, Encryption
 
- 
+enum SymmetricEncryptionAlgorithm:
+  case CaesarCipher, AES, DES, RSA
 
+enum HashFunctionAlgorithm:
+  case MD5, SHA1, SHA256, SHA384
 
+object SymmetricEncryptionAbstractFactory:
+  def apply[A>: Encryption ](name: SymmetricEncryptionAlgorithm): A =
+    name match {
+      case SymmetricEncryptionAlgorithm.CaesarCipher => CaesarCipher()
+      case SymmetricEncryptionAlgorithm.AES => AES()
+      case SymmetricEncryptionAlgorithm.DES => DES()
+    }
 
+object HashAbstractFactory:
+  def apply[A>: HashFunction](name: HashFunctionAlgorithm): A=
+    name match {
+      case HashFunctionAlgorithm.MD5 => new HashFunction.MD5
+      case HashFunctionAlgorithm.SHA1=> new HashFunction.SHA1
+      case HashFunctionAlgorithm.SHA256 => new HashFunction.SHA256
+      case HashFunctionAlgorithm.SHA384 => new HashFunction.SHA384
+    }
+
+object Prova:
+  def main(args: Array[String]): Unit =
+   val prova= SymmetricEncryptionAbstractFactory(SymmetricEncryptionAlgorithm.CaesarCipher)
+   val prova2:HashFunction=HashAbstractFactory(HashFunctionAlgorithm.MD5)
+   println("prova "+ prova)

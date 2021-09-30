dc8fdb535727184e3456d0b5af6fac45d87bb381
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2021-09-30 23:41:11 +0200

    mbf rename Caesar Cipher

---
+++ src/main/scala/it/unibo/authsim/library/dsl/cryptography/symmetric/CaesarCipher.scala
@@ -0,0 +1,29 @@
+package it.unibo.authsim.library.dsl.cryptography.symmetric
+
+import it.unibo.authsim.library.dsl.cryptography.symmetric.SymmetricEncryption
+import sun.security.util.Password
+
+trait CaesarCipher extends SymmetricEncryption
+
+object CaesarCipher:
+  def apply() = new CaesarCipher():
+    import it.unibo.authsim.library.dsl.cryptography.util.ImplicitConversion._
+
+    private var alpha=('a' to 'z') ++ ('1' to '9')
+
+    override def toString: String = "CaesarCipher"
+
+    override def encrypt[A,B](password: A, rotation: B): String = crypto(password, rotation)
+
+    override def decrypt[A,B](password: A, rotation: B): String = crypto(password,-rotation)
+
+    private def crypto(password: String, rotation:Int)=
+      password.toLowerCase.map {
+        case character if alpha.contains(character) => shift(alphaL, character, rotation)
+        case character => character
+      }
+
+    override def algorithmName: String = this.toString
+
+    private def shift(alpha:IndexedSeq[Char], character:Char, rotation:Int)=
+      alpha((character - alpha.head + rotation + alpha.size) % alpha.size)

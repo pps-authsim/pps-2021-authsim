878f7345155a6befeb079328185cbbd6c36c1a0e
Marica Pasquali <marica.pasquali@studio.unibo.it>
2021-10-01 23:41:46 +0200

    add documentations on CredentialPolicyGenerate

--- src/main/scala/it/unibo/authsim/library/dsl/policy/extractors/CredentialPolicyGenerate.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/policy/extractors/CredentialPolicyGenerate.scala
@@ -4,6 +4,10 @@ import it.unibo.authsim.library.dsl.policy.model.StringPolicies.*
 
 import scala.reflect.ClassTag
 
+/**
+ * ''CredentialPolicyGenerate'' is a trait used to generate an random string of the actual credential policy instance
+ * @tparam T actual type of credential policy
+ */
 trait CredentialPolicyGenerate[T: ClassTag]:
   def unapply(credentialPolicy: CredentialPolicy): Option[String] =
     credentialPolicy match {
@@ -13,6 +17,15 @@ trait CredentialPolicyGenerate[T: ClassTag]:
 
 
 object CredentialPolicyGenerate:
+  /**
+   * ''UserIDGenerate'' generates a random userID
+   */
   object UserIDGenerate extends CredentialPolicyGenerate[UserIDPolicy]
+  /**
+   * ''PasswordGenerate'' generates a random password
+   */
   object PasswordGenerate extends CredentialPolicyGenerate[PasswordPolicy]
+  /**
+   * ''OTPGenerate'' generates a random One Time Password
+   */
   object OTPGenerate extends CredentialPolicyGenerate[OTPPolicy]

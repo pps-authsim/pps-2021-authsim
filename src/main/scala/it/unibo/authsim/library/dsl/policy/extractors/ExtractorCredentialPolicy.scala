8d4d9469f23e11c238a27f9fc8a455ff820079a6
Marica Pasquali <marica.pasquali@studio.unibo.it>
2021-10-01 23:41:00 +0200

    add documentations on ExtractorCredentialPolicy

--- src/main/scala/it/unibo/authsim/library/dsl/policy/extractors/ExtractorCredentialPolicy.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/policy/extractors/ExtractorCredentialPolicy.scala
@@ -1,9 +1,14 @@
 package it.unibo.authsim.library.dsl.policy.extractors
 
+import it.unibo.authsim.library.dsl.policy.model.StringPolicies
 import it.unibo.authsim.library.dsl.policy.model.StringPolicies.*
 
 import scala.reflect.ClassTag
 
+/**
+ * ''ExtractorCredentialPolicy'' is trait used to extract the actual type of an [[CredentialPolicy credential policy]] instance
+ * @tparam T actual type of credential policy
+ */
 trait ExtractorCredentialPolicy[T: ClassTag]:
   def unapply(credentialPolicy: CredentialPolicy): Option[T] =
     credentialPolicy match {
@@ -12,6 +17,16 @@ trait ExtractorCredentialPolicy[T: ClassTag]:
     }
 
 object ExtractorCredentialPolicy:
+  /**
+   * ''UserIDPolicy'' extracts the [[StringPolicies.UserIDPolicy userID policy]] instance
+   *
+   */
   object UserIDPolicy extends ExtractorCredentialPolicy[UserIDPolicy]
+  /**
+   * ''PasswordPolicy'' extracts the [[StringPolicies.PasswordPolicy password policy]] instance
+   */
   object PasswordPolicy extends ExtractorCredentialPolicy[PasswordPolicy]
+  /**
+   * ''OTPPolicy'' extracts the [[StringPolicies.OTPPolicy otp policy]] instance
+   */
   object OTPPolicy extends ExtractorCredentialPolicy[OTPPolicy]

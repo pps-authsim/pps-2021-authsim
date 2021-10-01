008154e7572e9c8c865b4924561d48f84eef9efb
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2021-10-02 00:25:39 +0200

    documentation added

--- src/main/scala/it/unibo/authsim/library/dsl/cryptography/algorithm/EncryptionAlgorithm.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/cryptography/algorithm/EncryptionAlgorithm.scala
@@ -4,15 +4,36 @@ package it.unibo.authsim.library.dsl.cryptography.algorithm
  * Trait that represent an encryption algorithm
  */
 trait EncryptionAlgorithm:
+  /**
+   * Type value for the salt
+   */
   type Salt
   /**
-   * Method used to get the name of the crypthographic algorithm
+   * Getter for the name of the crypthographic algorithm
    * @return                        a string representing the name of crypthographic algorithm
    */
   def algorithmName: String
+
+  /**
+   * Getter for the salt used by the algorithm 
+   * 
+   * @return                        None if the algorithm does not use a salt value, or an optional of the salt value used by the algorithm
+   */
   def salt: Option[Salt]
+
+  /**
+   * Getter for the length of the key used by algorithm
+   * 
+   * @return                        the size of the used by the algorithm
+   */
   def keyLength: Int
 
+/**
+ * Trait that represent symmetric encryption algorithms
+ */
 trait SymmetricEncryptionAlgorithm extends EncryptionAlgorithm
 
+/**
+ * Trait that represent asymmetric encryption algorithms
+ */
 trait AsymmetricEncryptionAlgorithm extends EncryptionAlgorithm

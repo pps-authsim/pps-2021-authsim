c6eb40d2f92f567a3d3c302d0c5a718af9f939ef
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2021-10-02 09:57:15 +0200

    documentation added

--- src/main/scala/it/unibo/authsim/library/dsl/cryptography/util/ImplicitConversion.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/cryptography/util/ImplicitConversion.scala
@@ -1,21 +1,67 @@
 package it.unibo.authsim.library.dsl.cryptography.util
 
+/**
+ * Utility object to perform implicit conversions
+ */
 object ImplicitConversion:
+  /**
+   * Variable representing the charset to be used in encoding operations
+   */
   private val charset: String = "UTF8"
 
-  implicit def genericToString[A](inputObject: A): String = inputObject.toString
+  /**
+   * Method responsible of performing an implicit conversion from a generic to a string
+   * @param value           generic value to be converted
+   * @tparam A              type of the object
+   * @return                a string representing the input value
+   */
+  implicit def genericToString[A](value: A): String = value.toString
 
-  implicit def genericToInt [A] (inputObject: A): Int =
-    inputObject match {
+  /**
+   * Method responsible of performing an implicit conversion from a generic to an int
+   * @param value           generic value to be converted
+   * @tparam A              type of the object
+   * @return                the int value of the value if a conversion is possible, or 0 in any other case
+   */
+  implicit def genericToInt [A] (value: A): Int =
+    value match {
       case _: String =>
-        if(inputObject.toString.toIntOption.!=(None)) then
-          Integer.valueOf(inputObject.toString).intValue()
+        if(value.toString.toIntOption.!=(None)) then
+          Integer.valueOf(value.toString).intValue()
         else
           0
       case _=> 0
     }
 
+  /**
+   * Method responsible of performing an implicit conversion from a generic to a byte array
+   *
+   * @param value           generic value to be converted
+   * @tparam A              type of the object
+   * @return                a byte array representing the input value
+   */
   implicit def genericToArrayByte[A](value :A):Array[Byte] = stringToArrayByte(genericToString(value))
+
+  /**
+   * Method responsible of performing an implicit conversion from a string to a char array
+   *
+   * @param value           string value to be converted
+   * @return                a char array representing the input string
+   */
   implicit def stringToCharArray(value : String):Array[Char] =value.toCharArray
+
+  /**
+   * Method responsible of performing an implicit conversion from a string to a byte
+   *
+   * @param value           string value to be converted
+   * @return                a byte representing the input string
+   */
   implicit def stringToByte(value : String):Byte = Byte(value)
+
+  /**
+   * Method responsible of performing an implicit conversion from a string to a byte array
+   *
+   * @param value           string value to be converted
+   * @return                a byte array representing the input string
+   */
   implicit def stringToArrayByte(value : String):Array[Byte] =value.getBytes(charset)

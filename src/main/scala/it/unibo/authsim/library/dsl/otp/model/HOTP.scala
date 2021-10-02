82139be62182799216e6363acde0c3b0457a1474
Marica Pasquali <marica.pasquali@studio.unibo.it>
2021-10-02 12:47:31 +0200

    add documentations on OTP, HOTP and TOTP

--- src/main/scala/it/unibo/authsim/library/dsl/otp/model/HOTP.scala
+++ src/main/scala/it/unibo/authsim/library/dsl/otp/model/HOTP.scala
@@ -2,5 +2,11 @@ package it.unibo.authsim.library.dsl.otp.model
 
 import it.unibo.authsim.library.dsl.HashFunction
 
+/**
+ * ''HOTP'' rappresents an one time password (OTP) based on hash-based message authentication codes (HMAC).
+ */
 trait HOTP extends OTP:
+  /**
+   * @return a hash function used to generate the one time password
+   */
   def hashFunction: HashFunction

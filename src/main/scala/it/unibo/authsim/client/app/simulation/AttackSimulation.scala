0652b559784bb947875ad3254ce26ab0b80655f2
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2021-10-04 22:53:50 +0200

    add user provider init to attack simulation

--- src/main/scala/it/unibo/authsim/client/app/simulation/AttackSimulation.scala
+++ src/main/scala/it/unibo/authsim/client/app/simulation/AttackSimulation.scala
@@ -6,7 +6,9 @@ import it.unibo.authsim.client.app.mvvm.common.CredentialsSourceType
 import it.unibo.authsim.client.app.mvvm.model.attack.AttackSequence
 import it.unibo.authsim.client.app.mvvm.model.security.{CredentialsSource, SecurityPolicy}
 import it.unibo.authsim.client.app.simulation.exception.SimulationException
-import it.unibo.authsim.library.user.model.User
+import it.unibo.authsim.client.app.simulation.provider.RepositoryUserProvider
+import it.unibo.authsim.library.dsl.{HashFunction, UserProvider}
+import it.unibo.authsim.library.user.model.{CryptoInformation, User, UserInformation}
 import javafx.concurrent.Task
 
 import scala.collection.mutable.ListBuffer
@@ -19,25 +21,30 @@ class AttackSimulation(
                         val attackSequence: String
                       ) extends Task[Unit]:
 
+  private val database: UserRepository = credentialsSource match
+    case CredentialsSourceType.Sql => ComponentRegistry.userSqlRepository
+    case CredentialsSourceType.Mongo => ComponentRegistry.userMongoRepository
+
   override def call(): Unit =
+    // TODO make functional error handling (each try should be blocking)
     printInitialMessage()
-    // todo make pipeline with failure catching
-    var inserUsersResult = insertUsersIntoDatabase()
-    inserUsersResult match
-      case Failure(error) => printErrorMessage(error.getMessage)
-      case Success(value) => // TODO continue pipeline with match
+    val userProvider = makeUserProvider()
+    val insertUsersResult = insertUsersIntoDatabase()
+    printAttackInitializedMessage()
+    printAttackFinished()
 
+  private def makeUserProvider(): Try[UserProvider] = Try {
+    val matchedPolicy = SecurityPolicy.Default.all.find(policyToMatch => policyToMatch.policy.equals(policy))
+    matchedPolicy match
+      case Some(value) =>
+        val cryptoInformation = CryptoInformation(null)
+        val provider = new RepositoryUserProvider(database, cryptoInformation)
+        provider
+      case None => throw new SimulationException("Could not match provided policy with library policies")
+  }
 
-  def printInitialMessage(): Unit =
-    val initialText =  s"Starting an attack procedure '$attackSequence' with '$policy' security policy and '$credentialsSource' credentails source for users $users"
-    updateMessage(initialText)
-
-  def insertUsersIntoDatabase(): Try[Unit] = Try {
+  private def insertUsersIntoDatabase(): Try[Unit] = Try {
     val userEntities = users.map(user => new UserEntity(user.username, user.password)).toList
-
-    val database: UserRepository = credentialsSource match
-      case CredentialsSourceType.Sql => ComponentRegistry.userSqlRepository
-      case CredentialsSourceType.Mongo => ComponentRegistry.userMongoRepository
 
     var operationResult = database.saveUsers(userEntities)
     operationResult match
@@ -46,6 +53,20 @@ class AttackSimulation(
 
   }
 
-  def printErrorMessage(message:String) =
-    updateMessage(s"\nCould not launch attack, an error has occured: $message")
+  private def printErrorMessage(message:String) =
+    logMessage(s"Could not launch attack, an error has occured: $message...")
 
+  private def printInitialMessage(): Unit =
+    val initialText =  s"Starting an attack procedure '$attackSequence' with '$policy' security policy and '$credentialsSource' credentails source for users $users..."
+    logMessage(initialText)
+
+  private def printAttackInitializedMessage() =
+    val text = "Database and attack succesfully initialized..."
+    logMessage(text)
+
+  private def printAttackFinished() =
+    val text = "Attack simulation completed!"
+    logMessage(text)
+
+  private def logMessage(message: String) =
+    updateMessage(s"\n$message")

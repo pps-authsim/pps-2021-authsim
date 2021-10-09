fe0db53ad1e4aa1c6781730510f968b6d7bbba73
Alex Speranza <alex.speranza@studio.unibo.it>
2021-10-09 15:55:42 +0200

    Add bruteforce attack examples

--- src/main/scala/it/unibo/authsim/client/app/simulation/AttacksFactory.scala
+++ src/main/scala/it/unibo/authsim/client/app/simulation/AttacksFactory.scala
@@ -8,11 +8,26 @@ import it.unibo.authsim.library.dsl.consumers.StatisticsConsumer
 import it.unibo.authsim.library.dsl.cryptography.algorithm.hash.HashFunction
 import it.unibo.authsim.library.dsl.attack.builders.AttackBuilder
 import it.unibo.authsim.library.dsl.policy.alphabet.PolicyAlphabet.PolicyDefaultAlphabet
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.Duration
 
 class AttacksFactory(private val userProvider: UserProvider, private val logger: StatisticsConsumer):
 
-  private def defaultAlphabet: SymbolicAlphabet =
-    new PolicyDefaultAlphabet().alphanumericsymbols
+  private val defaultAlphabets = new PolicyDefaultAlphabet()
+  /**
+   * @return an AttackBuilder configured to attack a userProvider, using lowercase characters to construct the password string
+   *         with a maximum length of 6 and logging to the given logger, without timeout.
+   */
+  def bruteForceLowers(): AttackBuilder = new BruteForceAttackBuilder() against userProvider usingAlphabet defaultAlphabets.lowers logTo logger maximumLength 6
 
-  def bruteForceSimple(): AttackBuilder = new BruteForceAttackBuilder() against userProvider hashingWith HashFunction.MD5() usingAlphabet defaultAlphabet logTo logger
+  /**
+   * @return an AttackBuilder configured to attack a userProvider, using both lower and upper case characters to construct the password string
+   *         with a maximum length of 10 and logging to the given logger, with a timeout of 120 seconds (2 minutes).
+   */
+  def bruteForceLetters(): AttackBuilder = new BruteForceAttackBuilder() against userProvider usingAlphabet (defaultAlphabets.lowers and defaultAlphabets.uppers) maximumLength 10 logTo logger timeout Duration(120, TimeUnit.SECONDS)
 
+  /**
+   * @return an AttackBuilder configured to attack a userProvider, using all alphanumeric and symbols characters to construct the password string
+   *         with a maximum length of 16 and logging to the given logger, with a timeout of 600 seconds (10 minutes).
+   */
+  def bruteForceAll(): AttackBuilder = new BruteForceAttackBuilder() against userProvider usingAlphabet defaultAlphabets.alphanumericsymbols maximumLength 16 logTo logger timeout Duration(600, TimeUnit.SECONDS)

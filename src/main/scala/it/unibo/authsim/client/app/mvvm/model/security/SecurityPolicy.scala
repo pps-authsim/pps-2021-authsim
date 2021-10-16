3ed8f3226eab92a457475d34664e6beaa0fbdb30
Marica Pasquali <marica.pasquali@studio.unibo.it>
2021-10-16 17:46:53 +0200

    remove comment

--- src/main/scala/it/unibo/authsim/client/app/mvvm/model/security/SecurityPolicy.scala
+++ src/main/scala/it/unibo/authsim/client/app/mvvm/model/security/SecurityPolicy.scala
@@ -1,6 +1,5 @@
 package it.unibo.authsim.client.app.mvvm.model.security
 
-//import it.unibo.authsim.client.app.mvvm.model.security
 import it.unibo.authsim.library.cryptography.algorithm.CryptographicAlgorithm
 import it.unibo.authsim.library.policy.defaults.PolicyDefault
 import it.unibo.authsim.library.policy.model.Policy
